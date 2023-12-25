b19a7b4fd68e15b07572d712e08d8fc0b9dab85b
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-12-25 21:12:36 +0200

    Create first react components

---
+++ src/Greeting.jsx
@@ -0,0 +1,9 @@
+function Greeting() {
+  return <h1>&quot;I swear by my pretty floral bonnet</h1>;
+}
+
+function HeyPeople() {
+  return <div>hello people</div>;
+}
+
+export { Greeting, HeyPeople };
